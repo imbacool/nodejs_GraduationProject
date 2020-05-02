40a2645a8f6166e70120fe4ba49a8cc705ea44c5
ImbaCOOL <imbacool@189.cn>
2020-05-02 18:13:50 +0800

    0

--- controller/teachSchedule.js
+++ controller/teachSchedule.js
@@ -1,35 +1,24 @@
 const teachScheduleModel = require("../db/model/teachSchedule");
 
-// 添加教师课表
+// 增加教师课表
 let insertTeachSchedule = obj => teachScheduleModel.insertMany(obj);
 
-// // 删除教师课表
-// let deleteAdmin = _id => adminModel.deleteOne(_id);
+// 删除教师课表
+let deleteTeachSchedule = classID => teachScheduleModel.deleteOne(classID);
 
-// // 修改教师课表
-// let updateAdmin = (_id, obj) => adminModel.updateOne(_id, obj);
+// 修改教师课表
+let updateTeachSchedule = (classID, obj) => teachScheduleModel.updateOne(classID, obj);
 
-// // 修改教师课表状态
-// let changeState = (_id, state) => adminModel.updateOne(_id, state);
+// 查询教师课表
+let findTeachSchedule = ()=>teachScheduleModel.find();
 
-// // 查询教师课表(分页)
-// let findAdminByPage = async (page, pageSize) => {
-//     // 按照一定条件查询
-//     let data = await adminModel.find();
-//     // 查询到的数据的数量
-//     let count = data.length;
-//     // 按照一定条件分页
-//     let list = await adminModel.find().skip(Number((page - 1) * pageSize)).limit(Number(pageSize));
-
-//     return {
-//         count,
-//         list
-//     }
-// };
-
-// // 查询一个教师课表
-// let findAdminById = _id => adminModel.findOne(_id);
+// 查询一个教师课表(classID)
+let findByID = classID => teachScheduleModel.findOne(classID);
 
 module.exports = {
     insertTeachSchedule,
+    deleteTeachSchedule,
+    updateTeachSchedule,
+    findTeachSchedule,
+    findByID,
 };
--- router/teachSchedule.js
+++ router/teachSchedule.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const {
     insertTeachSchedule,
+    deleteTeachSchedule,
+    updateTeachSchedule,
+    findTeachSchedule,
+    findByID,
 } = require("../controller/teachSchedule");
 
 let router = express.Router();
@@ -27,123 +31,89 @@ router.post("/add", async (req, res) => {
     };
 });
 
-// // 删除教师课表
-// router.delete("/del", async (req, res) => {
-//     let {
-//         _id
-//     } = req.body;
+// 删除教师课表
+router.delete("/del", async (req, res) => {
+    let {
+        classID
+    } = req.body;
 
-//     try {
-//         await deleteGoods({
-//             _id
-//         });
-//         res.send({
-//             err: 0,
-//             msg: "ok"
-//         });
-//     } catch (err) {
-//         res.send({
-//             err: -1,
-//             msg: err
-//         });
-//     };
-// })
+    try {
+        await deleteTeachSchedule({
+            classID
+        });
+        res.send({
+            err: 0,
+            msg: "ok"
+        });
+    } catch (err) {
+        res.send({
+            err: -1,
+            msg: err
+        });
+    };
+})
 
-// // 修改教师课表
-// router.put("/update", async (req, res) => {
-//     let {
-//         _id,
-//         name,desc,price,num,type,img,state,date,hits,publisher
-//     } = req.body;
+// 修改教师课表
+router.put("/update", async (req, res) => {
+    let {
+        teacherID,name,classID,classNameCN,classNameEN,credits,classHours,classType,faculty,classInfo,classTime,classroom
+    } = req.body;
 
-//     try {
-//         await updateGoods({
-//             _id
-//         }, {
-//             name,desc,price,num,type,img,state,date,hits,publisher
-//         });
-//         res.send({
-//             err: 0,
-//             msg: "ok"
-//         });
-//     } catch (err) {
-//         res.send({
-//             err: -1,
-//             msg: err
-//         });
-//     };
-// })
+    try {
+        await updateTeachSchedule({
+            classID
+        }, {
+            teacherID,name,classNameCN,classNameEN,credits,classHours,classType,faculty,classInfo,classTime,classroom
+        });
+        res.send({
+            err: 0,
+            msg: "ok"
+        });
+    } catch (err) {
+        res.send({
+            err: -1,
+            msg: err
+        });
+    };
+})
 
-// // 修改教师课表状态
-// router.put("/state", async (req, res) => {
-//     let {
-//         _id,
-//         state
-//     } = req.body;
+// 查询教师课表
+router.get("/find", async (req, res) => {
+    try {
+        let data = await findTeachSchedule();
+        res.send({
+            err: 0,
+            msg: "ok",
+            data
+        });
+    } catch (err) {
+        res.send({
+            err: -1,
+            msg: err,
+        });
+    };
+})
 
-//     try {
-//         await changeState({
-//             _id
-//         }, {
-//             state
-//         });
-//         res.send({
-//             err: 0,
-//             msg: "ok"
-//         });
-//     } catch (err) {
-//         res.send({
-//             err: -1,
-//             msg: err
-//         });
-//     };
-// })
+// 查询一个教师课表(classID)
+router.get("/findByID", async (req, res) => {
+    let {
+        classID
+    } = req.query;
 
-// // 查询教师课表(分页)
-// router.get("/findByPage", async (req, res) => {
-//     let {
-//         page,
-//         pageSize
-//     } = req.query;
-
-//     try {
-//         let data = await findGoodsByPage(page,pageSize);
-//         res.send({
-//             err: 0,
-//             msg: "ok",
-//             count: data.count,
-//             list: data.list
-//         });
-//     } catch (err) {
-//         res.send({
-//             err: -1,
-//             msg: err,
-//             count: 0,
-//             list: []
-//         });
-//     };
-// })
-
-// // 查询一个教师课表
-// router.get("/findById", async (req, res) => {
-//     let {
-//         _id
-//     } = req.query;
-
-//     try {
-//         let data = await findGoodsById({_id});
-//         res.send({
-//             err: 0,
-//             msg: "ok",
-//             data
-//         });
-//     } catch (err) {
-//         res.send({
-//             err: -1,
-//             msg: err,
-//             data:{}
-//         });
-//     };
-// })
+    try {
+        let data = await findByID({classID});
+        res.send({
+            err: 0,
+            msg: "ok",
+            data
+        });
+    } catch (err) {
+        res.send({
+            err: -1,
+            msg: err,
+            data:{}
+        });
+    };
+})
 
 module.exports = router;
